d3a4fc32b0b52db613a1f4a1a345ea531114710e
BlueEvil777 <zecoltonisawesome@gmail.com>
2020-01-30 14:15:42 -0700

    Setup guided master ANT connection.

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -54,13 +54,18 @@ extern volatile u32 G_u32SystemTime1s;                    /*!< @brief From main.
 extern volatile u32 G_u32SystemFlags;                     /*!< @brief From main.c */
 extern volatile u32 G_u32ApplicationFlags;                /*!< @brief From main.c */
 
+extern u32 G_u32AntApiCurrentMessageTimeStamp;                    
+extern AntApplicationMessageType G_eAntApiCurrentMessageClass;    
+extern u8 G_au8AntApiCurrentMessageBytes[ANT_APPLICATION_MESSAGE_BYTES];  
+extern AntExtendedDataType G_sAntApiCurrentMessageExtData;                
 
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
 Variable names shall start with "UserApp1_<type>" and be declared as static.
 ***********************************************************************************************************************/
 static fnCode_type UserApp1_pfStateMachine;               /*!< @brief The state machine function pointer */
-//static u32 UserApp1_u32Timeout;                           /*!< @brief Timeout counter used across states */
+static AntAssignChannelInfoType UserApp1_sChannelInfo;
+static u32 UserApp1_u32Timeout;                           /*!< @brief Timeout counter used across states */
 
 
 /**********************************************************************************************************************
@@ -92,14 +97,42 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
-  /* If good initialization, set state to Idle */
-  if( 1 )
+  u8 au8WelcomeMessage[] = "ANT Master";
+
+  /* Set a message up on the LCD. Delay is required to let the clear command send. */
+  LcdCommand(LCD_CLEAR_CMD);
+  for(u32 i = 0; i < 10000; i++);
+  LcdMessage(LINE1_START_ADDR, au8WelcomeMessage);
+
+ /* Configure ANT for this application */
+  UserApp1_sChannelInfo.AntChannel          = ANT_CHANNEL_USERAPP;
+  UserApp1_sChannelInfo.AntChannelType      = ANT_CHANNEL_TYPE_USERAPP;
+  UserApp1_sChannelInfo.AntChannelPeriodLo  = ANT_CHANNEL_PERIOD_LO_USERAPP;
+  UserApp1_sChannelInfo.AntChannelPeriodHi  = ANT_CHANNEL_PERIOD_HI_USERAPP;
+ 
+  UserApp1_sChannelInfo.AntDeviceIdLo       = ANT_DEVICEID_LO_USERAPP;
+  UserApp1_sChannelInfo.AntDeviceIdHi       = ANT_DEVICEID_HI_USERAPP;
+  UserApp1_sChannelInfo.AntDeviceType       = ANT_DEVICE_TYPE_USERAPP;
+  UserApp1_sChannelInfo.AntTransmissionType = ANT_TRANSMISSION_TYPE_USERAPP;
+  UserApp1_sChannelInfo.AntFrequency        = ANT_FREQUENCY_USERAPP;
+  UserApp1_sChannelInfo.AntTxPower          = ANT_TX_POWER_USERAPP;
+
+  UserApp1_sChannelInfo.AntNetwork = ANT_NETWORK_DEFAULT;
+  for(u8 i = 0; i < ANT_NETWORK_NUMBER_BYTES; i++)
   {
-    UserApp1_pfStateMachine = UserApp1SM_Idle;
+    UserApp1_sChannelInfo.AntNetworkKey[i] = ANT_DEFAULT_NETWORK_KEY;
+  }
+  
+  /* Attempt to queue the ANT channel setup */
+  if( AntAssignChannel(&UserApp1_sChannelInfo) )
+  {
+    UserApp1_u32Timeout = G_u32SystemTime1ms;
+    UserApp1_pfStateMachine = UserApp1SM_AntChannelAssign;
   }
   else
   {
     /* The task isn't properly initialized, so shut it down and don't run */
+    DebugPrintf("Error\n");
     UserApp1_pfStateMachine = UserApp1SM_Error;
   }
 
@@ -151,7 +184,23 @@ static void UserApp1SM_Error(void)
   
 } /* end UserApp1SM_Error() */
 
-
+static void UserApp1SM_AntChannelAssign(void)
+{
+	if( AntRadioStatusChannel(ANT_CHANNEL_USERAPP) == ANT_CONFIGURED)
+  {
+    /* Channel assignment is successful, so open channel and
+    proceed to Idle state */
+    AntOpenChannelNumber(ANT_CHANNEL_USERAPP);
+    UserApp1_pfStateMachine = UserApp1SM_Idle;
+  }
+  
+  /* Watch for time out */
+  if(IsTimeUp(&UserApp1_u32Timeout, 3000))
+  {
+    DebugPrintf("Connect Fail\n");
+    UserApp1_pfStateMachine = UserApp1SM_Error;    
+  }
+}
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
